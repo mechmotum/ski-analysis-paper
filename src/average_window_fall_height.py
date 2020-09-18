b82b9a1640cd5af57baa0559d8457e690c4622a3
Jason K. Moore <moorepants@gmail.com>
2020-09-18 15:34:54 +0200

    Display floor height in meters.

--- src/average_window_fall_height.py
+++ src/average_window_fall_height.py
@@ -1,9 +1,13 @@
+"""Calculates the average floor height from data estimates taken from FIgure 1
+in Vish 2005."""
 import os
 import pandas as pd
 
 PATH_TO_THIS_FILE = os.path.abspath(__file__)
 DIR_OF_THIS_FILE = os.path.dirname(PATH_TO_THIS_FILE)
 PROJECT_ROOT = os.path.abspath(os.path.join(DIR_OF_THIS_FILE, '..'))
+
+ft2m = lambda x: 0.3048*x
 
 data = pd.read_csv(os.path.join(PROJECT_ROOT, 'data',
                                 'fig-1-data-vish-2005.csv'),
@@ -13,5 +17,5 @@ data['floor number'] = data['floor number'].astype(int)
 
 print('Count of data points for each floor:')
 print(data.groupby('floor number').count())
-print('Mean of data points for each floor:')
-print(data.groupby('floor number').mean())
+print('Mean of data points for each floor [meters]:')
+print(ft2m(data.groupby('floor number').mean()))
