ec506a574e2be5510f731690f688d99239d2b028
Jason K. Moore <moorepants@gmail.com>
2020-09-30 17:39:49 +0200

    Rotated the speed values above the bars.

--- src/case_study_figures.py
+++ src/case_study_figures.py
@@ -66,7 +66,7 @@ def compare_measured_to_designed(measured_surface, equiv_fall_height,
         height = rect.get_height()
         efh_ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
                     '{:1.1f}'.format(si), fontsize='xx-small', ha='center',
-                    va='bottom')
+                    va='bottom', rotation=90)
 
     dist, efh, speeds = landing.calculate_efh(np.deg2rad(takeoff_angle),
                                               takeoff.end, skier, increment)
